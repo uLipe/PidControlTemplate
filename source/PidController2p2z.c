7d115d7783d047f18c22f37f5a45a133b6ced585
uLipe <ryukokki.felipe@gmail.com>
2019-01-05 18:52:14 -0200

    pid_controllers: added method skeleton for controllers

--- source/PidController2p2z.c
+++ source/PidController2p2z.c
@@ -1,5 +1,16 @@
 #include <PidController2p2z.h>
 
+static float update (PidControllerInterface *self, float reference, float measured) {
+    
+}
+
+static void tune (PidControllerInterface *self, float kp, float ki, float kd) {
+
+}
+
+static void reset (PidControllerInterface *self) {
+
+}
 
 int PidControlller2p2zInit(Pid2p2z *self, PidControllerSettings *settings) {
     return 0;
--- source/PidControllerNumerical.c
+++ source/PidControllerNumerical.c
@@ -1,5 +1,17 @@
 #include <PidControllerNumerical.h>
 
+static float update (PidControllerInterface *self, float reference, float measured) {
+    
+}
+
+static void tune (PidControllerInterface *self, float kp, float ki, float kd) {
+
+}
+
+static void reset (PidControllerInterface *self) {
+
+}
+
 int PidControlllerNumericalInit(PidNumerical *self, PidControllerSettings *settings) {
     return 0;
 }
